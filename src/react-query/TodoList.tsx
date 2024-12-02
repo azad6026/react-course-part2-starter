df5395be80d2567c8ae410091356ec1576594935
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2024-12-03 09:19:52 +1100

    Updated todos with react query - draft

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,29 +1,33 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 interface Todo {
   id: number;
   title: string;
-  userId: number;
   completed: boolean;
 }
 
 const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
+  const fetchTodos = () =>
     axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => setTodos(res.data))
-      .catch((error) => setError(error));
-  }, []);
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.data);
+  const {
+    data: todos,
+    error,
+    isLoading,
+  } = useQuery<Todo[], Error>({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
 
-  if (error) return <p>{error}</p>;
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error.message}</p>;
 
   return (
     <ul className="list-group">
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <li key={todo.id} className="list-group-item">
           {todo.title}
         </li>
