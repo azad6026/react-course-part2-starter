7350bf01411c76eba44f843c173bba714fbcc420
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2024-12-03 11:34:14 +1100

    Updated posts by hook sepertion and addded user filtering

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,34 +1,32 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-}
+import { useState } from "react";
+import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => setPosts(res.data))
-      .catch((error) => setError(error));
-  }, []);
-
-  if (error) return <p>{error}</p>;
+  const [userId, setUsereId] = useState<number>();
+  const { data: posts, error, isLoading } = usePosts(userId);
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
 
   return (
-    <ul className="list-group">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select
+        onChange={(event) => setUsereId(parseInt(event.target.value))}
+        value={userId}
+        className="form-select mb-3"
+      >
+        <option value=""></option>
+        <option value="1">User1</option>
+        <option value="2">User2</option>
+        <option value="3">User3</option>
+      </select>
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
