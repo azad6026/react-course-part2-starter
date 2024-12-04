d4ee99a557f66831e629d04f821aacc1235bcb1f
Azadeh Faramarzi <aza.fara99@gmai.com.com>
2024-12-05 10:13:37 +1100

    Testing changes in th App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { useReducer } from "react";
 import "./App.css";
 import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
-
+import Counter from "./state-management/Counter";
+import LoginStatus from "./state-management/LoginStatus";
+import TaskList from "./state-management/TaskList";
+import taskReducer from "./state-management/reducers/tasksReducer";
+import TasksContext from "./state-management/contexts/tasksContext";
+import NavBar from "./state-management/NavBar";
+import HomePage from "./state-management/HomePage";
+import AuthContext from "./state-management/contexts/authContex";
+import loginStatusReducer from "./state-management/reducers/loginStatusReducer";
+import AuthProvider from "./state-management/AuthProvider";
 function App() {
+  const [tasks, tasksDispatch] = useReducer(taskReducer, []);
   return (
     <>
-      <TodoForm />
-      <TodoList />
+      {/* <Counter /> */}
+      {/* <TodoForm />
+      <TodoList /> */}
+      {/* <TaskList /> */}
+      {/* <LoginStatus /> */}
+      <AuthProvider>
+        <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
+          <HomePage />
+          <NavBar />
+        </TasksContext.Provider>
+      </AuthProvider>
     </>
   );
 }
